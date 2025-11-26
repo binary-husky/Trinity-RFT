704becdcbd4ef582d1ad491b27d90798d3fedb40
qingxu.fu <fuqingxu.fqx@alibaba-inc.com>
2025-11-26 16:25:32 +0800

    refactor: enhance PPO policy loss function with dual clipping mechanism

--- trinity/algorithm/policy_loss_fn/ppo_policy_loss.py
+++ trinity/algorithm/policy_loss_fn/ppo_policy_loss.py
@@ -6,6 +6,7 @@ Modified from https://github.com/volcengine/verl/blob/main/verl/trainer/ppo/core
 from typing import Dict, Optional, Tuple
 
 import torch
+import numpy as np
 
 from trinity.algorithm.policy_loss_fn.policy_loss_fn import POLICY_LOSS_FN, PolicyLossFn
 from trinity.algorithm.utils import masked_loss, masked_mean
@@ -42,19 +43,31 @@ class PPOPolicyLossFn(PolicyLossFn):
         advantages: torch.Tensor,
         **kwargs,
     ) -> Tuple[torch.Tensor, Dict]:
-        negative_approx_kl = logprob - old_logprob
-        ratio = torch.exp(negative_approx_kl)
-        ppo_kl = masked_mean(-negative_approx_kl, action_mask)
 
-        pg_losses = -advantages * ratio
-        pg_losses2 = -advantages * torch.clamp(
-            ratio, 1.0 - self.clip_range_low, 1.0 + self.clip_range_high  # type: ignore
-        )
-
+        E = logprob - old_logprob
+        E_clip = torch.zeros_like(E)
+        E_clip = torch.where(advantages > 0, torch.clamp(E, max=np.log(1.0+self.clip_range_high)), E_clip)
+        E_clip = torch.where(advantages < 0, torch.clamp(E, min=np.log(1.0-self.clip_range_low), max=np.log(5) ), E_clip)
+        ratio = torch.exp(E_clip)
+        print('using dual clip ppo loss')
+        pg_losses_comb = -advantages * ratio
         pg_loss = masked_loss(
-            torch.max(pg_losses, pg_losses2), action_mask, loss_agg_mode=self.loss_agg_mode
+            pg_losses_comb, action_mask, loss_agg_mode=self.loss_agg_mode
         )
-        pg_clipfrac = masked_mean(torch.gt(pg_losses2, pg_losses).float(), action_mask)
+
+        # negative_approx_kl = logprob - old_logprob
+        # ratio = torch.exp(negative_approx_kl)
+        ppo_kl = masked_mean(-E, action_mask)
+
+        # pg_losses = -advantages * ratio
+        # pg_losses2 = -advantages * torch.clamp(
+        #     ratio, 1.0 - self.clip_range_low, 1.0 + self.clip_range_high  # type: ignore
+        # )
+
+        # pg_loss = masked_loss(
+        #     torch.max(pg_losses, pg_losses2), action_mask, loss_agg_mode=self.loss_agg_mode
+        # )
+        pg_clipfrac = masked_mean((E_clip == E).float(), action_mask)
         metrics = {
             "pg_clipfrac": pg_clipfrac.detach().item(),
             "ppo_kl": ppo_kl.detach().item(),
